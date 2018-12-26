df9ec690b1c18fe59e9aeebd46729267b96df569
u2 <zhangyaning1985@gmail.com>
2018-12-26 10:17:26 +0800

    add more

--- clone-nightly-2017-12-05/src/main.rs
+++ clone-nightly-2017-12-05/src/main.rs
@@ -8,6 +8,26 @@ pub struct Stats {
    pub frequencies: [i32; 100],
 }
 
+#[derive(Clone)]
+pub struct Wrapper<T> {
+    pub inner: T,
+}
+
+pub struct SS<T> {
+    pub bar: Wrapper<T>,
+}
+
+// #[derive(Clone)]
+// struct Wrapper<T> {
+//     pub inner: T,
+// }
+//
+// failed
+// #[derive(Clone)]
+// struct SS<T> {
+//     pub bar: Wrapper<T>,
+// }
+
 fn main() {
     println!("Hello, world!");
 }
